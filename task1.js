1644320c58511904d370eae9b6e411960d6abd2f
Yuliia Ivaniuk <ivanykjula@gmail.com>
2025-04-07 23:42:19 +0300

    Solved tasks

--- task1.js
+++ task1.js
@@ -5,3 +5,20 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+const usersAPI = "https://jsonplaceholder.typicode.com/users";
+const usersList = document.querySelector('.usersList');
+
+async function getUsers(url, usersContainer) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const users = await response.json();
+    users.forEach(user => {
+        usersContainer.innerHTML += `<li>${user.name}</li>`;
+    })
+}
+
+getUsers(usersAPI, usersList);
--- task2.js
+++ task2.js
@@ -5,3 +5,26 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.querySelector('#userNameInput');
+const getUserBtn = document.getElementById('getUserButton');
+const userCity = document.querySelector('#userCity');
+
+async function getUserCity(url, name) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const users = await response.json();
+    const user = users.find(user => user.name === name);
+    if (user) {
+        userCity.textContent = `Місто користувача ${name}: ${user.address.city}`;
+    } else {
+        userCity.textContent = 'Користувача не знайдено';
+    }
+}
+
+getUserBtn.addEventListener('click', () => {
+    const name = userNameInput.value;
+    getUserCity('https://jsonplaceholder.typicode.com/users', name);
+});
